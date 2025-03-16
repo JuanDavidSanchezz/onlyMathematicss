c34177f8323187142c7849a757c884b3434a3e84
Juan Sanchez <juandavidsanchezrubiano@gmail.com>
2025-03-16 17:37:47 -0500

    Modificaciones

--- OnlyMathematics/src/main.jsx
+++ OnlyMathematics/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client' 
+import React from 'react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.Fragment>
+    <button>Hola Mundo</button>
+  </React.Fragment> 
 )
